67b1dcf279d8cbf956d2e503ae6bf089e6dcee3d
King Elisha <ek.chibueze@gmail.com>
2019-06-04 16:39:08 +0100

    db ready

--- src/quote.js
+++ src/quote.js
@@ -20,24 +20,24 @@ let db = new sqlite3.Database(DBSOURCE, (err) => {
     throw err
   } else {
     console.log('Connected to the SQLite database.')
-    db.run(`CREATE TABLE quotes (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        category text,
-        quote text,
-        length text,
-        author text
-        )`,
-      (err) => {
-        if (err) {
-          // Table already created
-        } else {
-          // Table just created, creating some rows
-          var insert = 'INSERT INTO quotes (category, quote, length, author) VALUES (?,?,?,?)'
-          var quotes = ["Today I choose life. Every morning when I wake up I can choose joy, happiness, negativity, pain... To feel the freedom that comes from being able to continue to make mistakes and choices - today I choose to feel life, not to deny my humanity but embrace it", "Your success and happiness lies in you. Resolve to keep happy, and your joy and you shall form an invincible host against difficulties"];
-          db.run(insert, ["happiness", quotes[0], getLength(quotes[0].length) , "Kevyn Aucoin"])
-          db.run(insert, ["happiness", quotes[1], getLength(quotes[1].length), "Helen Keller"])
-        }
-      });
+    // db.run(`CREATE TABLE quotes (
+    //     id INTEGER PRIMARY KEY AUTOINCREMENT,
+    //     category text,
+    //     quote text,
+    //     length text,
+    //     author text
+    //     )`,
+    //   (err) => {
+    //     if (err) {
+    //       // Table already created
+    //     } else {
+    //       // Table just created, creating some rows
+    //       var insert = 'INSERT INTO quotes (category, quote, length, author) VALUES (?,?,?,?)'
+    //       var quotes = ["Today I choose life. Every morning when I wake up I can choose joy, happiness, negativity, pain... To feel the freedom that comes from being able to continue to make mistakes and choices - today I choose to feel life, not to deny my humanity but embrace it", "Your success and happiness lies in you. Resolve to keep happy, and your joy and you shall form an invincible host against difficulties"];
+    //       db.run(insert, ["happiness", quotes[0], getLength(quotes[0].length) , "Kevyn Aucoin"])
+    //       db.run(insert, ["happiness", quotes[1], getLength(quotes[1].length), "Helen Keller"])
+    //     }
+    //   });
   }
 });
 

72b1a881d40e0ac5887f19ace7fd70efabc29229
King Elisha <ek.chibueze@gmail.com>
2019-06-04 16:19:53 +0100

    added length column

--- src/quote.js
+++ src/quote.js
@@ -13,6 +13,7 @@ let db = new sqlite3.Database(DBSOURCE, (err) => {
     //     id INTEGER PRIMARY KEY AUTOINCREMENT,
     //     category text,
     //     quote text,
+    //     length INTEGER,
     //     author text
     //     )`,
     //   (err) => {
@@ -20,9 +21,10 @@ let db = new sqlite3.Database(DBSOURCE, (err) => {
     //       // Table already created
     //     } else {
     //       // Table just created, creating some rows
-    //       var insert = 'INSERT INTO quotes (category, quote, author) VALUES (?,?,?)'
-    //       db.run(insert, ["Happiness", "Today I choose life. Every morning when I wake up I can choose joy, happiness, negativity, pain... To feel the freedom that comes from being able to continue to make mistakes and choices - today I choose to feel life, not to deny my humanity but embrace it", "Kevyn Aucoin"])
-    //       db.run(insert, ["Happiness", "Your success and happiness lies in you. Resolve to keep happy, and your joy and you shall form an invincible host against difficulties", "Helen Keller"])
+    //       var insert = 'INSERT INTO quotes (category, quote, length, author) VALUES (?,?,?,?)'
+    //       var quotes = ["Today I choose life. Every morning when I wake up I can choose joy, happiness, negativity, pain... To feel the freedom that comes from being able to continue to make mistakes and choices - today I choose to feel life, not to deny my humanity but embrace it", "Your success and happiness lies in you. Resolve to keep happy, and your joy and you shall form an invincible host against difficulties"];
+    //       db.run(insert, ["Happiness", quotes[0], quotes[0].length, "Kevyn Aucoin"])
+    //       db.run(insert, ["Happiness", quotes[1], quotes[1].length, "Helen Keller"])
     //     }
     //   });
   }
